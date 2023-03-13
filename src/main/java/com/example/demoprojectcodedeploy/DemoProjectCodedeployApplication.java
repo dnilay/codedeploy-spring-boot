ce4a8c46b936ee8da04c2042b9e53d3c0a742eb3
Nilay <mailnilaydas@gmail.com>
2023-03-13 15:13:57 +0530

    1st commit

---
+++ src/main/java/com/example/demoprojectcodedeploy/DemoProjectCodedeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoprojectcodedeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectCodedeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectCodedeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoprojectcodedeploy/DemoProjectCodedeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoprojectcodedeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectCodedeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
